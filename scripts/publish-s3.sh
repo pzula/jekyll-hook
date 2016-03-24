8c5bfd53767055da9073ad88e277043cdb32afd0
Persa Zula <zula@adobe.com>
2016-03-24 18:33:21 -0400

    Sync site directly into the root of s3 bucket

--- scripts/publish-s3.sh
+++ scripts/publish-s3.sh
@@ -11,16 +11,8 @@ owner=$3
 giturl=$4
 source=$5
 build=$6
-# S3 bucket
 bucket=$7
 rubypath=$8
 
-  # add repo name to _sites
-  touch /tmp/$repo
-  aws s3 cp /tmp/$repo s3://$bucket/_sites/
-  rm /tmp/$repo
-
-  # if not root repo, sync to subdirectory
-  aws s3 sync $build/ s3://$bucket/$repo/ --delete
-
-fi
+# sync site to deployment bucket
+aws s3 sync $build/ s3://$bucket/ --delete
